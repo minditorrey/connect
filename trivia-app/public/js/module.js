1dbf403e2aaf60f550e7739ec3c832420a19c68c
Mindi Torrey <minditorrey@me.com>
2016-05-02 23:40:05 -0700

    add cards, get cards by category functionality

---
+++ trivia-app/public/js/module.js
@@ -0,0 +1,35 @@
+var app = angular.module('triviaApp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+		.state('categories', {
+			url: '/categories',
+			templateUrl: '/templates/categories.html',
+			controller: 'categoriesController'
+		})
+		.state('dragDetails', {
+			url: '/dragDetails',
+			templateUrl: '/templates/dragDetails.html',
+			controller: 'dragDetailsController'
+		})
+		.state('bobDetails', {
+			url: '/bobDetails',
+			templateUrl: '/templates/bobDetails.html',
+			controller: 'bobDetailsController'
+		})
+		.state('deadDetails', {
+			url: '/deadDetails',
+			templateUrl: '/templates/deadDetails.html',
+			controller: 'deadDetailsController'
+		})
+		.state('cards', {
+			url: '/cards/:category',
+			templateUrl: '/templates/cards.html',
+			controller: 'cardsController',
+		})
+
+
+	$urlRouterProvider.otherwise('/');
+
+});
