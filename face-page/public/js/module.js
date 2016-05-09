1b4ea751a6f2d5e6047445872a70fdfc52e9634a
Mindi Torrey <minditorrey@me.com>
2016-05-09 16:28:32 -0700

    first commit

---
+++ face-page/public/js/module.js
@@ -0,0 +1,43 @@
+var app = angular.module('facePageApp', ['ui.router']);
+
+app.run(function(Auth) {
+  Auth.getProfile();
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: '/templates/home.html',
+			controller: 'homeCtrl'
+		})
+		.state('register', {
+			url: '/register',
+			templateUrl: '/templates/authForm.html',
+			controller: 'authFormCtrl'
+		})
+		.state('login', {
+      		url: '/login',
+      		templateUrl: '/templates/authForm.html',
+      		controller: 'authFormCtrl'
+    	})
+		.state('profile', {
+      		url: '/profile',
+      		templateUrl: '/templates/profile.html',
+      		controller: 'profileCtrl',
+      		resolve: {
+        		profile: function(Auth, $q, $state) {
+          			return Auth.getProfile()
+          			.catch(() => {
+           			$state.go('home');
+            		return $q.reject();
+          			});
+        		}
+      		}
+    	})
+
+
+	$urlRouterProvider.otherwise('/');
+
+});
