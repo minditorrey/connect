9da5e8be021ca65f971b0fe0b97beb0b3f8386b9
Mindi Torrey <minditorrey@me.com>
2016-05-24 10:12:53 -0700

    first commit

---
+++ connect/public/js/controllers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = angular.module('finalApp');
+
+app.controller('homeController', function($scope) {
+    console.log('homeCtrl!');
+
+});
+
+app.controller('profilesController', function($scope, $state, $rootScope) {
+    console.log('profileCtrl!');
+    console.log('user:', $scope.user);
+
+    var username = $rootScope.user.username;
+    console.log('username', username);
+    $scope.user = $rootScope.user;
+    $scope.profilePic = $rootScope.user.customData.profilePic;
+
+  });
